07a9818f62b7d32f33bda814b9b5f872617c51ce
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-13 23:10:29 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,9 @@ require 'pry'
 def remove_strawberry(contacts)
 contacts.each do |name,data|
 data.each do |attribute,value|
-
+  if attribute == :favorite_ice_cream_flavors
+    value.delete("strawberry")
+  end 
 end
 end
 end
